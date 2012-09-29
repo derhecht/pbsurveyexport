bfa12634d10b44cc7ab2cde83cf5b0bd6d753855
patrick <patrick@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-09-29 12:15:37 +0000

    [FEATURE] Possibility to delete answers and results
    There is no possibility to delete answers and results when they are exported to a csv file. There is a demand for it, for instance when having the same survey for different groups, or for deleting test results.
    
    This patch adds a new function to the module called "Delete results"
    
    Fixes: #41461
    
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/pbsurveyexport/trunk@66442 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,13 +1,13 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext: "pbsurveyexport"
+# Extension Manager/Repository config file for ext "pbsurveyexport".
 #
-# Auto generated 01-06-2007 14:14
+# Auto generated 29-09-2012 12:28
 #
 # Manual updates:
-# Only the data in the array - anything else is removed by next write.
-# "version" and "dependencies" must not be touched!
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
@@ -15,22 +15,23 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Export module for Questionaire extension.',
 	'category' => 'module',
 	'shy' => 0,
-	'dependencies' => 'pbsurvey',
+	'version' => '1.0.6',
+	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
+	'loadOrder' => '',
 	'module' => '',
 	'state' => 'stable',
-	'internal' => 0,
 	'uploadfolder' => 0,
 	'createDirs' => '',
 	'modify_tables' => '',
-	'clearCacheOnLoad' => 0,
+	'clearcacheonload' => 0,
 	'lockType' => '',
 	'author' => 'Patrick Broens',
 	'author_email' => 'patrick@patrickbroens.nl',
 	'author_company' => '',
-	'version' => '1.0.5',
-	'_md5_values_when_last_written' => 'a:9:{s:9:"Changelog";s:4:"7f3f";s:12:"ext_icon.gif";s:4:"ee26";s:15:"ext_php_api.dat";s:4:"dfe5";s:14:"ext_tables.php";s:4:"d60c";s:13:"locallang.xml";s:4:"fe2d";s:26:"csh/locallang_modfunc1.xml";s:4:"ca89";s:29:"csh/nl.locallang_modfunc1.xml";s:4:"0343";s:27:"lang/locallang_modfunc1.xml";s:4:"cd52";s:45:"modfunc1/class.tx_pbsurveyexport_modfunc1.php";s:4:"bb3f";}',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
 			'pbsurvey' => '',
@@ -42,8 +43,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'suggests' => array(
-	),
+	'_md5_values_when_last_written' => 'a:9:{s:9:"Changelog";s:4:"7f3f";s:12:"ext_icon.gif";s:4:"ee26";s:15:"ext_php_api.dat";s:4:"dfe5";s:14:"ext_tables.php";s:4:"d60c";s:13:"locallang.xml";s:4:"fe2d";s:26:"csh/locallang_modfunc1.xml";s:4:"ca89";s:29:"csh/nl.locallang_modfunc1.xml";s:4:"0343";s:27:"lang/locallang_modfunc1.xml";s:4:"cd52";s:45:"modfunc1/class.tx_pbsurveyexport_modfunc1.php";s:4:"4f01";}',
 );
 
 ?>
--- ext_tables.php
+++ ext_tables.php
@@ -1,13 +1,26 @@
 <?php
-if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
+if (!defined ('TYPO3_MODE')) {
+	die ('Access denied.');
+}
 
-if (TYPO3_MODE=='BE')	{
+if (TYPO3_MODE == 'BE') {
+	// Adds the "Function menu module" ('third level module') "Export results" to the existing function menu for the pbsurvey backend module
 	t3lib_extMgm::insertModuleFunction(
 		'web_txpbsurveyM1',
 		'tx_pbsurveyexport_modfunc1',
-		t3lib_extMgm::extPath($_EXTKEY).'modfunc1/class.tx_pbsurveyexport_modfunc1.php',
+		t3lib_extMgm::extPath($_EXTKEY) . 'modfunc1/class.tx_pbsurveyexport_modfunc1.php',
 		'LLL:EXT:pbsurveyexport/lang/locallang_modfunc1.xml:moduleFunction'
 	);
+	// Adds the "Function menu module" ('third level module') "Delete results" to the existing function menu for the pbsurvey backend module
+	t3lib_extMgm::insertModuleFunction(
+		'web_txpbsurveyM1',
+		'tx_pbsurveyexport_modfunc2',
+		t3lib_extMgm::extPath($_EXTKEY) . 'modfunc2/class.tx_pbsurveyexport_modfunc2.php',
+		'LLL:EXT:pbsurveyexport/lang/locallang_modfunc2.xml:moduleFunction'
+	);
 }
-t3lib_extMgm::addLLrefForTCAdescr('_MOD_web_txpbsurveyM1','EXT:pbsurveyexport/csh/locallang_modfunc1.xml');
-?>
+
+// Adds a reference for module "Export results" to a locallang file with $GLOBALS['TCA_DESCR'] labels
+t3lib_extMgm::addLLrefForTCAdescr('_MOD_web_txpbsurveyM1', 'EXT:pbsurveyexport/csh/locallang_modfunc1.xml');
+// Adds a reference for module "Delete results" to a locallang file with $GLOBALS['TCA_DESCR'] labels
+t3lib_extMgm::addLLrefForTCAdescr('_MOD_web_txpbsurveyM2', 'EXT:pbsurveyexport/csh/locallang_modfunc2.xml');
