045e6629e0c0a3afff7a5c2d5eb22575ee30e3f1
patrick <patrick@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-11-21 14:08:35 +0000

    [TASK] Version number changed from 1.0.6 to 1.1.0 after upload to TER
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/pbsurveyexport/trunk@81683 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,22 +1,22 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext "pbsurveyexport".
-#
-# Auto generated 29-09-2012 12:28
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
+/***************************************************************
+ * Extension Manager/Repository config file for ext "pbsurveyexport".
+ *
+ * Auto generated 21-11-2013 15:03
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
 
-$EM_CONF[$_EXTKEY] = array(
+$EM_CONF[$_EXTKEY] = array (
 	'title' => 'Questionaire export',
 	'description' => 'Export module for Questionaire extension.',
 	'category' => 'module',
 	'shy' => 0,
-	'version' => '1.0.6',
-	'dependencies' => '',
+	'version' => '1.1.0',
+	'dependencies' => 'pbsurvey',
 	'conflicts' => '',
 	'priority' => '',
 	'loadOrder' => '',
@@ -32,18 +32,25 @@ $EM_CONF[$_EXTKEY] = array(
 	'author_company' => '',
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
-	'constraints' => array(
-		'depends' => array(
+	'constraints' => 
+	array (
+		'depends' => 
+		array (
 			'pbsurvey' => '1.0.0-1.5.99',
 			'php' => '4.0.0-5.4.99',
 			'typo3' => '3.8.1-6.1.99',
 		),
-		'conflicts' => array(
+		'conflicts' => 
+		array (
 		),
-		'suggests' => array(
+		'suggests' => 
+		array (
 		),
 	),
 	'_md5_values_when_last_written' => 'a:9:{s:9:"Changelog";s:4:"7f3f";s:12:"ext_icon.gif";s:4:"ee26";s:15:"ext_php_api.dat";s:4:"dfe5";s:14:"ext_tables.php";s:4:"d60c";s:13:"locallang.xml";s:4:"fe2d";s:26:"csh/locallang_modfunc1.xml";s:4:"ca89";s:29:"csh/nl.locallang_modfunc1.xml";s:4:"0343";s:27:"lang/locallang_modfunc1.xml";s:4:"cd52";s:45:"modfunc1/class.tx_pbsurveyexport_modfunc1.php";s:4:"4f01";}',
+	'suggests' => 
+	array (
+	),
 );
 
 ?>
